b5324e159e1a90e07e18d2b931351841f456b680
Masato Haruta <haruta@328w.co.jp>
2016-03-07 15:44:17 +0900

    add files

---
+++ app/src/main/java/com/toolbar/jp/editorapplication/EditorHoge.java
@@ -0,0 +1,13 @@
+package com.toolbar.jp.editorapplication;
+
+public class EditorHoge {
+    private String mHoge;
+
+    public EditorHoge(String hoge) {
+        mHoge = hoge;
+    }
+
+    public String getHoge() {
+        return mHoge;
+    }
+}
